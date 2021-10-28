3f338f3d5046aeeafefa12c88e610028560efe06
menegueli <gabriel.menegueli.pereira@gmail.com>
2021-10-28 11:51:54 -0300

    primeiro commit

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+      //Colocar icone dos componentes, vue, nuxt e tailwind
+    <footer>Portfólio - Gabriel Menegueli Projeto Desenvolvido com Vue.JS/Nuxt.JS/TailwindCSS</footer>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-row items-center justify-around p-4 bg-red-600">
+    <img class="w-12 flex-row" src="../static/pokebola.png" alt="pokebola">
+    <img class="w-28 flex-row" src="../static/pokemon.png" alt="pokemon">
+    <h1 class="text-lg text-white font-semibold">PokeVue.JS</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
